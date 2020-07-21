7a6c266e05dd1269f76f594a2d54a638607105b1
Tomasz Stachowiak <h3@h3.gd>
2020-07-21 20:02:52 +0200

    make example more illustrative

--- examples/add.rs
+++ examples/add.rs
@@ -12,24 +12,32 @@ impl LazyWorker for Add {
     type Output = i32;
 
     async fn run(self, _: LazyContext) -> Result<Self::Output> {
-        dbg!(self.identity());
+        println!("Running Add({}, {})", self.a, self.b);
         Ok(self.a + self.b)
     }
 }
 
 fn main() -> Result<()> {
-    let add1 = Add { a: 5, b: 7 }.into_lazy();
-    let add2 = Add { a: 5, b: 7 }.into_lazy();
-    let add3 = add2.clone();
-
     let cache = Cache::create();
     let mut runtime = Runtime::new()?;
 
-    dbg!(*runtime.block_on(add1.eval(&cache))?);
-    dbg!(*runtime.block_on(add2.eval(&cache))?);
-    dbg!(*runtime.block_on(add3.eval(&cache))?);
-    dbg!(*runtime.block_on(Add { a: 5, b: 7 }.into_lazy().eval(&cache))?);
-    dbg!(*runtime.block_on(Add { a: 5, b: 8 }.into_lazy().eval(&cache))?);
+    {
+        let add1 = Add { a: 5, b: 7 }.into_lazy();
+        let add2 = Add { a: 5, b: 7 }.into_lazy();
+        let add3 = add2.clone();
+
+        dbg!(*runtime.block_on(add1.eval(&cache))?);
+        dbg!(*runtime.block_on(add2.eval(&cache))?);
+        dbg!(*runtime.block_on(add3.eval(&cache))?);
+        dbg!(*runtime.block_on(Add { a: 5, b: 7 }.into_lazy().eval(&cache))?);
+        dbg!(*runtime.block_on(Add { a: 5, b: 8 }.into_lazy().eval(&cache))?);
+    }
+
+    // Lose the above references, thus removing them from the cache
+
+    {
+        dbg!(*runtime.block_on(Add { a: 5, b: 7 }.into_lazy().eval(&cache))?);
+    }
 
     Ok(())
 }
