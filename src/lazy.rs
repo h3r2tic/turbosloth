36427c76dfd72b7eba19365ae96135d6aaa72981
Tomasz Stachowiak <h3@h3.gd>
2020-07-21 13:45:13 +0200

    remove all mutation and rebinding

--- src/lazy.rs
+++ src/lazy.rs
@@ -2,12 +2,11 @@ use crate::cache::*;
 
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use std::marker::PhantomData;
 use std::{
     any::Any,
     future::Future,
     pin::Pin,
-    sync::{Arc, Mutex, RwLock},
+    sync::{Arc, RwLock},
 };
 
 pub trait LazyReqs: Any + Sized + Send + 'static {}
@@ -48,12 +47,12 @@ where
 pub trait LazyWorkerObj<T: LazyReqs>: LazyWorker<Output = T> + LazyWorkerEx<T> {}
 impl<T: LazyReqs, W> LazyWorkerObj<T> for W where W: LazyWorker<Output = T> + LazyWorkerEx<T> {}
 
-pub struct LazyOwnedPayload<T: LazyReqs> {
+pub struct LazyPayload<T: LazyReqs> {
     pub worker: Box<dyn LazyWorkerObj<T>>,
     pub build_result: Arc<BuildResult<T>>,
 }
 
-impl<T: LazyReqs> Clone for LazyOwnedPayload<T> {
+impl<T: LazyReqs> Clone for LazyPayload<T> {
     fn clone(&self) -> Self {
         Self {
             worker: self.worker.clone_boxed().into(),
@@ -63,7 +62,7 @@ impl<T: LazyReqs> Clone for LazyOwnedPayload<T> {
 }
 
 pub struct BuildResult<T: LazyReqs> {
-    value: RwLock<Arc<Option<T>>>,
+    value: RwLock<Option<Arc<T>>>,
 }
 
 impl<T: LazyReqs> Default for BuildResult<T> {
@@ -78,42 +77,10 @@ impl<T: LazyReqs> BuildResult<T> {
     fn is_none(&self) -> bool {
         self.value.read().unwrap().is_none()
     }
-
-    fn is_some(&self) -> bool {
-        self.value.read().unwrap().is_some()
-    }
-}
-
-pub struct LazyPrevPayload<T: LazyReqs> {
-    pub prev_worker: Mutex<Option<Box<dyn LazyWorkerObj<T>>>>,
-    pub build_result: Arc<BuildResult<T>>,
-}
-
-impl<T: LazyReqs> Clone for LazyPrevPayload<T> {
-    fn clone(&self) -> Self {
-        Self {
-            prev_worker: Mutex::new(None),
-            build_result: Default::default(),
-        }
-    }
-}
-
-pub enum LazyValuePayload<T: LazyReqs> {
-    Actual(LazyOwnedPayload<T>),
-    Prev(LazyPrevPayload<T>),
-}
-
-impl<T: LazyReqs> Clone for LazyValuePayload<T> {
-    fn clone(&self) -> Self {
-        match self {
-            Self::Actual(v) => Self::Actual(v.clone()),
-            Self::Prev(v) => Self::Prev(v.clone()),
-        }
-    }
 }
 
 pub struct Lazy<T: LazyReqs> {
-    pub payload: LazyValuePayload<T>,
+    pub payload: LazyPayload<T>,
     pub identity: u64,
 }
 
@@ -121,10 +88,6 @@ impl<T: LazyReqs + Sync> Lazy<T> {
     pub fn shared(self) -> Arc<Lazy<T>> {
         Arc::new(self)
     }
-
-    /*pub fn rebind<RetVal: ToLazy>(&mut self, _rebind_fn: impl Fn(Self) -> RetVal) {
-        todo!();
-    }*/
 }
 
 impl<T: LazyReqs> Clone for Lazy<T> {
@@ -136,57 +99,6 @@ impl<T: LazyReqs> Clone for Lazy<T> {
     }
 }
 
-pub trait LazyFeedback<T: LazyReqs> {
-    fn feedback(self) -> (Arc<Lazy<T>>, LazyFeedbackTarget<T>);
-}
-
-impl<T: LazyReqs + Sync> LazyFeedback<T> for Lazy<T> {
-    fn feedback(self) -> (Arc<Lazy<T>>, LazyFeedbackTarget<T>) {
-        if let LazyValuePayload::Actual(payload) = self.payload {
-            let prev = Arc::new(Lazy {
-                payload: LazyValuePayload::Prev(LazyPrevPayload {
-                    build_result: payload.build_result.clone(),
-                    prev_worker: Mutex::new(Some(payload.worker)),
-                }),
-                identity: self.identity,
-            });
-
-            let actual = LazyFeedbackTarget {
-                identity: self.identity,
-                prev: Lazy {
-                    payload: LazyValuePayload::Prev(LazyPrevPayload {
-                        build_result: payload.build_result,
-                        prev_worker: Mutex::new(None),
-                    }),
-                    identity: self.identity,
-                },
-            };
-
-            (prev, actual)
-        } else {
-            panic!("feedback() can only be called on owned lazy references")
-        }
-    }
-}
-
-pub struct LazyFeedbackTarget<T: LazyReqs> {
-    identity: u64,
-    prev: Lazy<T>,
-}
-
-impl<T: LazyReqs + Sync> LazyFeedbackTarget<T> {
-    pub fn rebind<RetVal>(self, rebind_fn: impl Fn(Lazy<T>) -> RetVal) -> Lazy<T>
-    where
-        RetVal: ToLazy,
-        RetVal: LazyWorker<Output = ()>,
-    {
-        //todo!();
-        // TODO
-        let res = rebind_fn(self.prev).lazy();
-        todo!()
-    }
-}
-
 pub trait EvalLazy<T: LazyReqs> {
     type Output;
     fn eval(
@@ -195,153 +107,54 @@ pub trait EvalLazy<T: LazyReqs> {
     ) -> Pin<Box<dyn Future<Output = Result<Self::Output>> + Send + 'static>>;
 }
 
-// Could be used for getting exclusive access to a non-shared Lazy<T>.
-// By denying `Send`, the mutable variable needs to be accessed last in the worker,
-// Thus making it more difficult to mutate it, or trying to lock it before other
-// variables have been evaluated. If they used `prev` values of the variable
-// that's about to be mutated, those will get the correct value.
-//
-// Not really enforceable, since T could be Arc<Rwlock<_>>, and the user
-// can still shoot themselves in the foot with `.write()`, and enjoy deadlocks.
-#[derive(Debug)]
-pub struct NoSend<T>(T, PhantomData<*const ()>);
-
-impl<T> std::ops::Deref for NoSend<T> {
-    type Target = T;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<T> std::ops::DerefMut for NoSend<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-// TODO: validate at runtime that a shared ref does not exist for this item
 impl<T: LazyReqs> EvalLazy<T> for Lazy<T> {
-    type Output = NoSend<T>;
+    type Output = T;
 
     fn eval(
         self,
         cache: &Cache,
     ) -> Pin<Box<dyn Future<Output = Result<Self::Output>> + Send + 'static>> {
-        /*let payload = Arc::try_unwrap(self.payload)
-        .ok()
-        .expect("Lazy::eval: more than one reference exists!");*/
-
-        let payload = if let LazyValuePayload::Actual(payload) = self.payload {
-            payload
-        } else {
-            panic!("An owned Lazy value must contain LazyValuePayload::Actual")
-        };
-
-        let worker = payload.worker;
-        /*let worker = Arc::into_raw(payload.worker);
-        let worker: Box<dyn LazyWorkerObj<T>> = unsafe { Box::from_raw(worker as *mut _) };*/
-
+        let worker = self.payload.worker;
         let cache = cache.clone();
 
         Box::pin(async move {
             let worker = worker.run_boxed(cache);
             let res: T = tokio::task::spawn(worker).await??;
-            Ok(NoSend(res, PhantomData))
+            Ok(res)
         })
     }
 }
 
-pub struct LazyValueReadRef<T: LazyReqs>(Arc<Option<T>>);
-pub struct LazyValueWriteRef<T: LazyReqs>(Arc<T>);
-
-//unsafe impl<T: LazyReqs + Send> Send for LazyValueReadRef<T> {}
-impl<T: LazyReqs> std::ops::Deref for LazyValueReadRef<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        (&*self.0).as_ref().unwrap()
-    }
-}
-
 impl<T: LazyReqs + Sync> EvalLazy<T> for Arc<Lazy<T>> {
-    type Output = LazyValueReadRef<T>;
+    type Output = Arc<T>;
 
     fn eval(
         self,
         cache: &Cache,
     ) -> Pin<Box<dyn Future<Output = Result<Self::Output>> + Send + 'static>> {
-        match &self.payload {
-            // Shared ref
-            LazyValuePayload::Actual(payload) => {
-                // TODO: run worker if necessary
+        // HACK; TODO: check if build result doesn't exist or is stale
+        if self.payload.build_result.is_none() {
+            let worker = self.payload.worker.clone_boxed();
+            let build_result = self.payload.build_result.clone();
+            let cache = cache.clone();
 
-                /*let worker = payload.worker.clone_boxed();
-                let db = cache.0.clone();
+            Box::pin(async move {
+                let worker = worker.run_boxed(cache);
+                let res: T = tokio::task::spawn(worker).await??;
+                *build_result.value.write().unwrap() = Some(Arc::new(res));
 
-                let cached_val = {
-                    let db_vals = db.values.read().unwrap();
-                    db_vals.get(&self.identity).cloned()
-                };
-
-                let cache = cache.clone();
-                let identity = self.identity;
-
-                Ok(Box::pin(async move {
-                    if let Some(cached) = cached_val {
-                        Ok(cached
-                            .downcast::<T>()
-                            .map_err(|e| anyhow!("Could not downcast cached value: {:?}", e))?)
-                    } else {
-                        let worker = worker.run_boxed(cache);
-                        let res: Arc<T> = Arc::new(tokio::task::spawn(worker).await??);
-                        let mut db_vals = db.values.write().unwrap();
-                        db_vals.insert(identity, res.clone());
-                        Ok(res)
-                    }
-                }))*/
-
-                // HACK; TODO: check if build result doesn't exist or is stale
-                if payload.build_result.is_none() {
-                    let worker = payload.worker.clone_boxed();
-                    let build_result = payload.build_result.clone();
-                    let cache = cache.clone();
-
-                    Box::pin(async move {
-                        let worker = worker.run_boxed(cache);
-                        let res: T = tokio::task::spawn(worker).await??;
-                        *Arc::get_mut(&mut build_result.value.write().unwrap()).unwrap() =
-                            Some(res);
-
-                        let v: Self::Output =
-                            LazyValueReadRef(build_result.value.read().unwrap().clone());
-
-                        Ok(v)
-                    })
+                let v: Option<Arc<T>> = build_result.value.read().unwrap().clone();
+                if let Some(v) = v {
+                    Ok(v)
                 } else {
-                    if payload.build_result.is_some() {
-                        let v: Self::Output =
-                            LazyValueReadRef(payload.build_result.value.read().unwrap().clone());
-
-                        Box::pin(async move { Ok(v) })
-                    } else {
-                        Box::pin(async move { Err(anyhow!("The requested asset failed to build")) })
-                    }
+                    Err(anyhow!("The requested asset failed to build"))
                 }
-            }
-            LazyValuePayload::Prev(payload) => {
-                if payload.build_result.is_some() {
-                    let v: Self::Output =
-                        LazyValueReadRef(payload.build_result.value.read().unwrap().clone());
-
-                    Box::pin(async move { Ok(v) })
-                } else {
-                    Box::pin(async move {
-                        Err(anyhow!(
-                            "A previous value for a lazy reference is not available."
-                        ))
-                    })
-                }
-            }
+            })
+        } else {
+            let v: Self::Output =
+                Option::<&Arc<_>>::cloned(self.payload.build_result.value.read().unwrap().as_ref())
+                    .expect("a valid build result");
+            Box::pin(async move { Ok(v) })
         }
     }
 }
@@ -353,10 +166,10 @@ where
     fn lazy(self) -> Lazy<<Self as LazyWorker>::Output> {
         Lazy {
             identity: self.identity(),
-            payload: LazyValuePayload::Actual(LazyOwnedPayload {
+            payload: LazyPayload {
                 worker: Box::new(self),
                 build_result: Default::default(),
-            }),
+            },
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -53,31 +53,6 @@ impl LazyWorker for Add {
     }
 }
 
-#[derive(Clone)]
-struct MutAdd {
-    a: Lazy<i32>,
-    b: Arc<Lazy<i32>>,
-}
-
-impl ToLazy for MutAdd {
-    fn identity(&self) -> u64 {
-        self.a.identity * 12345 + self.b.identity // TODO: hash
-    }
-}
-
-#[async_trait]
-impl LazyWorker for MutAdd {
-    type Output = ();
-
-    async fn run(self, cache: Cache) -> Result<Self::Output> {
-        let b = self.b.clone().eval(&cache).await?;
-        let mut a = self.a.eval(&cache).await?;
-        println!("running MutAdd({}, {})", *a, *b);
-        *a += *b;
-        Ok(())
-    }
-}
-
 fn try_main() -> Result<()> {
     let a = 1i32.lazy().shared();
     let b = 2i32.lazy().shared();
@@ -86,33 +61,8 @@ fn try_main() -> Result<()> {
     let cache = CacheDb::create();
     let mut runtime = Runtime::new()?;
 
-    // Should return two references:
-    // A shared immutable ref to the previous value. Its purpose is to say "you can read
-    // the previous value, but you don't own it, and can't rebind it." This reference
-    // should only be valid until the second returned value is used.
-    // The purpose of the second returned value is to allow rebinding the temporal resource.
-    // There's probably no way to make this work with static verification, so runtime checks.
-    // Note: mutation is not allowed for any of the lazy values. The way GPU rendering
-    // with in-place changes could work is through interior mutability, relying on the
-    // locking inherent in using a single render/compute queue.
-    let (f_prev, f_next) = 0i32.lazy().feedback();
-
-    // Note: f_prev must be used within the eval tree of f below. Otherwise
-    // it might be evaluated out of order, and get the latest value instead of the prev one.
-    let d = Add {
-        a: c.clone(),
-        b: f_prev,
-    }
-    .lazy();
-
-    let f = f_next.rebind(|feedback| MutAdd {
-        a: feedback,
-        b: 1i32.lazy().shared(),
-    });
-
-    dbg!(*runtime.block_on(c.eval(&cache))?);
-    dbg!(runtime.block_on(d.eval(&cache))?);
-    dbg!(runtime.block_on(f.clone().eval(&cache))?);
+    dbg!(*runtime.block_on(c.clone().eval(&cache))?);
+    dbg!(*runtime.block_on(c.clone().eval(&cache))?);
 
     Ok(())
 }
@@ -125,33 +75,4 @@ fn main() {
             .for_each(|cause| eprintln!("because: {}", cause));
         std::process::exit(1);
     }
-}
-
-// Two kinds of feedback:
-// * mutable ref: in-place update a resource
-//     * rebind nominal value without creating a copy
-//     * quite special; can't include itself in eval tree
-// * double-backed: current and previous values are distinct instances
-//     * TAA and company
-//     * regular temporal loop; publishes a new value in every iteration
-
-// Can we use the Rust borrow checker to validate usage?
-
-struct Thing {
-    ver: i32,
-}
-
-fn mut_thing(a: &mut Thing) {
-    a.ver += 1;
-}
-
-fn use_thing(a: &Thing) {}
-
-fn proto() {
-    let mut a = Thing { ver: 0 };
-    mut_thing(&mut a);
-    use_thing(&a);
-    mut_thing(&mut a);
-    use_thing(&a);
-    use_thing(&a);
 }
